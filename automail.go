a412ad040e54b74eba7c3d4e882f9b1694a21680
gunduzy19 <imgxslash@gmail.com>
2023-03-05 16:27:30 +0300

    bug fix

--- automail.go
+++ automail.go
@@ -15,10 +15,13 @@ import (
 
 func main() {
 
-	receivers := getReceivers()
-	for _, receiver := range receivers {
-		sendGoMail(receiver)
-		break
+	receivers := getReceivers("participants.xlsx")
+	for index, receiver := range receivers {
+		if index > 80 {
+			fmt.Println(fmt.Sprintf("Sending to %s with email %s", receiver.Name, receiver.Email))
+			fmt.Println(index)
+			sendGoMail(receiver)
+		}
 	}
 }
 
@@ -34,20 +37,15 @@ type Mail struct {
 	Body    string
 }
 
-func readExcel() *excelize.File {
+func readExcelCols(filePath string) [][]string {
 
-	f, err := excelize.OpenFile("participants.xlsx")
+	f, err := excelize.OpenFile(filePath)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return f
-}
-
-func readExcelCols() [][]string {
-
-	col, err := readExcel().GetCols("Form Responses 1")
+	col, err := f.GetCols("Form Responses 1")
 
 	if err != nil {
 		log.Fatal(err)
@@ -56,23 +54,23 @@ func readExcelCols() [][]string {
 	return col
 }
 
-func getReceivers() []Receiver {
+func getReceivers(filePath string) []Receiver {
 
-	col := readExcelCols()
+	col := readExcelCols(filePath)
 
 	receivers := make([]Receiver, len(col[1]), len(col[1]))
 	for index, name := range col[1] {
 		if index == 0 {
 			continue
 		}
-		receivers[index-1].Name = name
+		receivers[index-1].Name = strings.TrimSpace(name)
 	}
 
 	for index, email := range col[3] {
 		if index == 0 {
 			continue
 		}
-		receivers[index-1].Email = email
+		receivers[index-1].Email = strings.TrimSpace(email)
 	}
 	return receivers
 }
@@ -87,43 +85,30 @@ func prepareBody(name string) string {
 		log.Fatal(err)
 	}
 
-	//headers := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";"
-
 	return body.String()
 }
 
 func sendGoMail(receiver Receiver) {
-	sender := "qsbitu22@gmail.com"
-
-	to := []string{
-		"yunus.e.gunduz@gmail.com",
-	}
-
-	// to := []string{receiver.Email}
-
 	user := "qsbitu22@gmail.com"
 	password := "ihcduscobatgsyzn"
 
-	subject := "QSB ITU Python for Quantum Computing Certificate"
-	body := prepareBody(receiver.Name)
-
 	request := Mail{
-		Sender:  sender,
-		To:      to,
-		Subject: subject,
-		Body:    body,
+		Sender:  "qsbitu22@gmail.com",
+		To:      []string{receiver.Email},
+		Subject: "QSB ITU Python for Quantum Computing Certificate",
+		Body:    prepareBody(receiver.Name),
 	}
 
 	addr := "smtp.gmail.com:587"
 	host := "smtp.gmail.com"
 
-	fmt.Println(receiver.Name)
 	filePath := fmt.Sprintf("./certificates/%s.pdf", strings.TrimSpace(receiver.Name))
 	data := buildMail(request, filePath)
 	auth := smtp.PlainAuth("", user, password, host)
-	err := smtp.SendMail(addr, auth, sender, to, data)
+	err := smtp.SendMail(addr, auth, request.Sender, request.To, data)
 
 	if err != nil {
+		fmt.Println(receiver.Name)
 		log.Fatal(err)
 	}
 	fmt.Println("Email sent successfully")
